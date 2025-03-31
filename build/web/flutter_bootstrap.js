52d28ce4c86f95a9b5a6686f28461cdd04bd7a9a
XLK36769 <UVVortex@gmail.com>
2025-03-31 22:33:01 +0200

    Updated title and meta description

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "924662051"
+    serviceWorkerVersion: "602583277"
   }
 });
